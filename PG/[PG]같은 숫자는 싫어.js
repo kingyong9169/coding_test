3c186e1247b05154c0fc88895676b311b3c27bc1
bruney <drdragon777@naver.com>
2021-10-26 22:17:05 +0900

    [PG]같은 숫자는 싫어 - Lv.1

---
+++ PG/[PG]같은 숫자는 싫어.js
@@ -0,0 +1,8 @@
+function solution(arr){
+    let answer = [arr[0]];
+    for(let i = 0;i < arr.length;i++){
+        if(answer[answer.length - 1] !== arr[i])
+            answer.push(arr[i]);
+    }
+    return answer;
+}
