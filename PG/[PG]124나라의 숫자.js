9336ded3adc9cdf32df5c10a8b91493ac5b51c8f
동동이 <drdragon777@naver.com>
2021-12-26 20:16:23 +0900

    [PG]124나라의 숫자 - Lv.2

---
+++ PG/[PG]124나라의 숫자.js
@@ -0,0 +1,24 @@
+function solution(n) { // 처음 내 풀이
+    let isPrevZero = false;
+    let nextNum = '';
+    n = n.toString(3);
+    for(let i = n.length - 1 ; i >= 0 ; i--){
+        if(isPrevZero){
+            if(n[i] == 0) nextNum = '2';
+            else if(n[i] == 1) nextNum = '4'
+            if(n[i] == 2 || i === 0){
+                nextNum = Number(n[i]) - 1 + '';
+                isPrevZero = false;
+            }
+            n = n.slice(0, i) + nextNum + n.slice(i + 1);
+            continue;
+        }
+        if(n[i] == 0){
+            n = n.slice(0, i) + '4' + n.slice(i + 1);
+            isPrevZero = true;
+        }
+        else isPrevZero = false;
+    }
+    
+    return Number(n) + '';
+}
