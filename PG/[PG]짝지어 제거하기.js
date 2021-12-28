93c8b11201f85e5aa654cad47ed1b0c875b15df8
동동이 <drdragon777@naver.com>
2021-12-28 13:25:15 +0900

    [PG]짝지어 제거하기 - Lv.2 오류 코드 추가

--- PG/[PG]짝지어 제거하기.js
+++ PG/[PG]짝지어 제거하기.js
@@ -1,3 +1,17 @@
+function solution(s){ // 효율성 1개 통과
+    if(s.length % 2 === 1) return 0;
+    
+    for(let i = 0 ; i < s.length - 1 ; i++){
+        if(s[i] === s[i + 1]){
+            s = s.slice(0, i) + s.slice(i + 2);
+            if(i > 0) i = i - 2;
+            else i--;
+        }
+    }
+    
+    return s.length ? 0 : 1;
+}
+
 function solution(s){
     if(s.length % 2 === 1) return 0;
     
