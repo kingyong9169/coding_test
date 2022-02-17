3389249d643e5ba4b61e89a0bd1267b3e93a5b76
동동이 <drdragon777@naver.com>
2022-02-17 23:33:38 +0900

    [PG] 매칭 점수 - 다른 사람 풀이

--- PG/Lv.3/[PG]매칭 점수.js
+++ PG/Lv.3/[PG]매칭 점수.js
@@ -1,4 +1,4 @@
-function solution(word, pages) {
+function solution(word, pages) { // 내 풀이
     let max = 0;
     const reg = new RegExp(',' + word + ',', 'gi');
     const urlReg = new RegExp(/https:\/\/[^"\s]+/);
@@ -27,4 +27,36 @@ function solution(word, pages) {
     
     basic.forEach((x, i) => max = x[3] > basic[max][3] ? i : max);
     return max;
+}
+
+function solution(word, rawPages) { // 다른 사람 풀이
+    word = word.toLowerCase();
+    return rawPages
+        .map((rawPage, index) => {
+            const [_, url] = rawPage.match(/<meta property="og:url" content="([^"]+)"\/>/i);
+            const tags = rawPage.match(/<[^>]+>/g);
+            const basicScore = tags
+                .reduce((raw, tag) => raw.replace(tag, ""), rawPage)
+                .split(/[^a-zA-Z]/)
+                .filter(w => w.toLowerCase() == word).length;
+            const outUrls = tags.reduce((links, tag) => {
+                const result = tag.match(/<a href="([^"]+)">/i);
+                return result ? links.concat(result[1]) : links;
+            }, []);
+
+            return {
+                index,
+                url,
+                outUrls,
+                basicScore,
+            };
+        })
+        .map((page, index, pages) => {
+            page.linkScore = pages
+                .filter(p => p.outUrls.some(url => url == page.url))
+                .reduce((sum, p) => sum + p.basicScore/p.outUrls.length, 0);
+            page.totalScore = page.linkScore + page.basicScore;
+            return page;
+        })
+        .sort((a,b) => a.totalScore == b.totalScore ? a.index-b.index : b.totalScore - a.totalScore)[0].index;
 }
