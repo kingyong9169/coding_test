b80f5168814fc969f05404de600adeb89df4ae20
동동이 <drdragon777@naver.com>
2023-04-14 22:36:17 +0900

    [PG] 숫자 블록 - Lv.2 문제 바뀜

--- PG/Lv.2/[PG]숫자 블록.js
+++ PG/Lv.2/[PG]숫자 블록.js
@@ -18,3 +18,23 @@ function solution(begin, end) {
   }
   return result;
 }
+
+function getMax(num) {
+  if (num === 1) return 0;
+  const result = [];
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      result.push(i);
+      if (num / i <= num) return num / i;
+    }
+  }
+  return result.length ? result[result.length - 1] : 1;
+}
+
+function solution(begin, end) {
+  const answer = [];
+  for (let i = begin; i <= end; i++) {
+    answer.push(getMax(i));
+  }
+  return answer;
+}
