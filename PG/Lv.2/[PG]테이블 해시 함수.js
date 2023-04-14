48bdd70e729d2e2a8af68cdf8bfce057e2a0191d
동동이 <drdragon777@naver.com>
2023-04-14 16:30:24 +0900

    [PG] 테이블 해시 함수 - 리팩토링

--- PG/Lv.2/[PG]테이블 해시 함수.js
+++ PG/Lv.2/[PG]테이블 해시 함수.js
@@ -1,9 +1,6 @@
 function solution(data, col, row_begin, row_end) {
   let answer = 0;
-  data.sort((a, b) => {
-    if (a[col - 1] !== b[col - 1]) return a[col - 1] - b[col - 1];
-    return b[0] - a[0];
-  });
+  data.sort((a, b) => a[col - 1] - b[col - 1] || b[0] - a[0]);
 
   for (let i = row_begin; i <= row_end; i++) {
     const cur = data[i - 1].reduce((acc, cur) => acc + (cur % i), 0);
